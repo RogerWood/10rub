a6aa43ed8c03b96997096bbbaa2aa0003386a6ce
Roger Wood <not.roger.wood@gmail.com>
2011-09-19 10:13:24 +0400

    Добавлен шаблон

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,1 +1,11 @@
+# умные ссылки
 include Nanoc3::Helpers::LinkTo
+
+# поддержка частичных шаблонов
+include Nanoc3::Helpers::Rendering
+
+# теги
+Nanoc3::Helpers::Tagging
+
+# карта сайта для поисковых систем
+include Nanoc3::Helpers::XMLSitemap
