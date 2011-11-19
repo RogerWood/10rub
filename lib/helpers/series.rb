1e562817f8d3f1627b015106ad0ec4fb61c6f3e7
RogerWood <not.roger.wood@gmail.com>
2011-11-19 22:41:29 +0400

    Обновлена структура главной страницы

--- lib/helpers/series.rb
+++ lib/helpers/series.rb
@@ -2,16 +2,18 @@ module Nanoc3::Helpers
 
    module Series
       
-      # количество серий монет
+      #
+      # количество серий
+      #
+
       def number_of_series
 
-         # директории . и .. заранее необходимо убрать
-         number = -2
+         number = 0
          init_dir = "content/series/"
-
+         
          files = Dir.entries(init_dir)
          
-         files.each do |file|
+         files[2..files.size].each do |file|
             number += 1 if FileTest.directory?(init_dir + file)
          end
 
@@ -19,12 +21,71 @@ module Nanoc3::Helpers
 
       end
 
+      #
       # склонение для количества серий
+      #
+
       def tail_series
 
          number = number_of_series
 
          tails = Array[ "серия монет", "серии монет", "серий монет" ]
+         cases = Array[ 2, 0, 1, 1, 1, 2 ]
+         
+         tails[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[Array[number % 10, 5].min] ]
+
+      end
+
+      # возвращает численное значение количества монет в коллекции. 
+      # определение происходит по полю 'дата покупки' в файле *.yaml
+      # fixme: на данный момент определение происходит по полю 'дата выпуска'
+
+      def number_of_coins
+
+         number = 0
+         init_dir = "content/series/"
+         
+         files = Dir.entries(init_dir)
+
+         # перебор каталогов серий
+         files[2..files.size].each do |file|
+            dir = init_dir + file + '/'
+
+            if FileTest.directory?(dir)
+               
+               # перебор возможных файлов монет
+               coins = Dir.entries(dir)
+
+               coins[2..coins.size].each do |coin|
+                  if FileTest.file?(dir + coin) and coin.downcase.include?('.yaml')
+                     
+                     # загрузка файла *.yaml
+                     f = File.new(dir + coin)
+                     coin_data = YAML.load(f)
+                     f.close
+                     
+                     # признак присутствия в коллекции — поле 'дата выпуска' (fixme: должно быть 'дата покупки')
+                     number += 1 if coin_data['date_of_issue']
+
+                  end
+               end
+
+            end
+         end
+
+         number
+
+      end
+
+      #
+      # склонение для количества монет
+      #
+
+      def tail_coins
+
+         number = number_of_series
+
+         tails = Array[ "монета", "монет", "монеты" ]
          cases = Array[ 2, 0, 1, 1, 1, 2 ]
          
          tails[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[Array[number % 10, 5].min] ]
