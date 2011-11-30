cfc3ba18ae7928b72b414f87ab0ddf0ee6986257
RogerWood <not.roger.wood@gmail.com>
2011-11-30 22:05:51 +0400

    Упрощен алгоритм определения количество серий. Серии формируются автоматически.

--- lib/helpers/series.rb
+++ lib/helpers/series.rb
@@ -1,25 +1,6 @@
 module Nanoc3::Helpers
 
    module Series
-      
-      #
-      # количество серий
-      #
-
-      def number_of_series
-
-         number = 0
-         init_dir = "content/series/"
-         
-         files = Dir.entries(init_dir)
-         
-         files[2..files.size].each do |file|
-            number += 1 if FileTest.directory?(init_dir + file)
-         end
-
-         number
-
-      end
 
       #
       # склонение для количества серий
@@ -27,7 +8,7 @@ module Nanoc3::Helpers
 
       def tail_series
 
-         number = number_of_series
+         number = @site.articles.size
 
          tails = Array[ "серия монет", "серии монет", "серий монет" ]
          cases = Array[ 2, 0, 1, 1, 1, 2 ]
@@ -83,9 +64,9 @@ module Nanoc3::Helpers
 
       def tail_coins
 
-         number = number_of_series
+         number = number_of_coins
 
-         tails = Array[ "монета", "монет", "монеты" ]
+         tails = Array[ "монета", "монеты", "монет" ]
          cases = Array[ 2, 0, 1, 1, 1, 2 ]
          
          tails[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[Array[number % 10, 5].min] ]
