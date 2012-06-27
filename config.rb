52a881b2ba79ee26f925da0f04219ea60ad1a04c
RogerWood <not.roger.wood@gmail.com>
2012-06-27 22:05:43 +0400

    Большой апдейт

---
+++ config.rb
@@ -0,0 +1,9 @@
+# конфигурационный файл для Compass
+
+project_path = File.dirname(__FILE__)
+http_path    = '/'
+output_style = :expanded #:compressed
+
+css_dir      = 'output/assets/css'
+images_dir   = 'output/assets/images'
+sass_dir     = 'content/assets/css'
